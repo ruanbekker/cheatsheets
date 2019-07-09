80963f776ac9fb341db8ddbf517ef5284bcf31ff
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-07-09 12:00:20 +0200

    Create delete_item.py

---
+++ dynamodb/python/delete_item.py
@@ -0,0 +1,14 @@
+import boto3
+
+client = boto3.Session(region_name='eu-west-1').client('dynamodb', aws_access_key_id='', aws_secret_access_key='', endpoint_url='http://localhost:4567')
+
+
+response = client.delete_item(
+    Key={
+        'event': {'S': 'gaming_nationals_zaf'},
+        'timestamp': {'S': '2019-02-08T14:53'}
+    },
+    TableName='gamescores'
+)
+
+print(response)
