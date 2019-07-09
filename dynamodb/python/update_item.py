a969808877cb1cfb7ec3db6c1b4b345f05938d6a
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-07-09 11:59:28 +0200

    Create update_item.py

---
+++ dynamodb/python/update_item.py
@@ -0,0 +1,16 @@
+import boto3
+
+client = boto3.Session(region_name='eu-west-1').client('dynamodb', aws_access_key_id='', aws_secret_access_key='', endpoint_url='http://localhost:4567')
+
+response = client.update_item(
+    TableName='gamescores',
+    Key={
+        'event': {'S': 'gaming_nationals_zaf'},
+        'timestamp': {'S': '2019-02-08T14:53'}
+    },
+    AttributeUpdates={
+        'gamerid': {'Value': {'S': 'willx9335'}}
+    }
+)
+
+print(response)
