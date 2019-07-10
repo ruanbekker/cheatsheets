aadb73434d4b5c4c227d86f8da2fbbe71ca18758
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-07-10 11:50:03 +0200

    Create slack_helper.py

---
+++ slack/python/slack_helper.py
@@ -0,0 +1,17 @@
+# pip install slackclient
+# https://github.com/JPStrydom/Crypto-Trading-Bot/issues/10
+
+from slack import WebClient as SlackClient
+legacy_token = "" # https://api.slack.com/custom-integrations/legacy-tokens
+slack_client = SlackClient(legacy_token)
+
+def list_channels():
+    name_to_id = {}
+    res = slack_client.api_call(
+        "groups.list", # groups are private channels, conversations are public channels. Different API.
+    )
+    list_channels = {"private_channels": {}}
+    for channel in res['groups']:
+        list_channels['private_channels'] = {channel['name']: channel['id']}
+    return list_channels
+        
