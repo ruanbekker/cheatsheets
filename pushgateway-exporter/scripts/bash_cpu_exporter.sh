ede9c25be662264c3b698bb56f3549726b76c5e5
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-09-18 16:57:52 +0200

    Create bash_cpu_exporter.sh

---
+++ pushgateway-exporter/scripts/bash_cpu_exporter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This exporter gets stats from top and sends it to pushgateway
+# Prometheus will scrape the pushgateway service and consumes to prometheus
+# Grafana will visualize the data
+
+# resource:
+# https://devconnected.com/monitoring-linux-processes-using-prometheus-and-grafana/
+
+job_name="top"
+my_hostname="my-ec2-instance"
+exporter_type="bash"
+auth_string="x:x"
+pushgateway_url="pushgateway.mydomain.com"
+
+z=$(ps aux)
+while read -r z
+do
+   var=$var$(awk '{print "exporter_cpu_usage{process=\""$11"\", pid=\""$2"\"}", $3z}');
+done <<< "$z"
+curl -X POST -u "$auth_string" -H  "Content-Type: text/plain" --data "$var
+" https://$pushgateway_url/metrics/job/$job_name/instance/$my_hostname/exporter_type/$exporter_type
