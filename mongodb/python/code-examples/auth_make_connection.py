a4e17306c10b28a2690da8214492a81e114faa22
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-10-03 22:54:44 +0200

    Update auth_make_connection.py

--- mongodb/python/code-examples/auth_make_connection.py
+++ mongodb/python/code-examples/auth_make_connection.py
@@ -1,4 +1,6 @@
 # https://pymongo.readthedocs.io/en/stable/examples/authentication.html?highlight=authentication
+# example server: 
+# - https://github.com/ruanbekker/cheatsheets/blob/master/mongodb/python/docker/docker-compose.yml
 
 from pymongo import MongoClient
 client = MongoClient("192.168.0.8:27017", username="root", password="pass", authSource="admin", authMechanism="SCRAM-SHA-256")
