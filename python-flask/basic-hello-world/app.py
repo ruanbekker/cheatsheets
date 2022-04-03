94a503d097d0a7bfc0041995185a419d98096e96
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-04-03 09:20:05 +0200

    create basic flask hello world

---
+++ python-flask/basic-hello-world/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'hello world'
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
